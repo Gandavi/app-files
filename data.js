78eb2aee2359213a4c1729e9f581a231dba649e2
Gandavi <113764120+Gandavi@users.noreply.github.com>
2024-07-31 23:20:32 +0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1_--",
+      "name": "1_Храм Архангела Гавриила",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.4341208605456242,
+        "pitch": -0.06382423692383377,
+        "fov": 1.399027273370374
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.838782386557309,
+          "pitch": -0.0874056906676497,
+          "rotation": 0,
+          "target": "1-2_---------"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1549382914400255,
+          "pitch": -0.09041667053351077,
+          "title": "Title",
+          "text": "TextText"
+        }
+      ]
+    },
+    {
+      "id": "1-2_---------",
+      "name": "2_Приход Святых Мучениц Веры, Надежды, Любови и матери их Софии",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 2.838538662752205,
+        "pitch": -0.08068138575013961,
+        "fov": 1.399027273370374
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
